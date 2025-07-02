dcb5658fb4a60e172531ecf0836922d99d2e0353
Saksham <83531898+mashug0@users.noreply.github.com>
2025-07-03 03:56:09 +0530

    Added Basic Execution File

---
+++ mm.py
@@ -0,0 +1,22 @@
+from PIL import Image
+import numpy as np
+from torchvision import transforms
+from torchvision.transforms.functional import to_pil_image, to_tensor
+import cv2
+from Dataset.Degrador import Degrador
+from Dataset.CreateDataset import CreateDataset
+import os
+from torch.utils.data import DataLoader
+
+dataset = CreateDataset(
+    HR_dir="Data\\Spacenet\\HR",
+    LR1_dir="Data\\Spacenet\\LR1",
+    LR2_dir="Data\\Spacenet\\LR2"
+)
+
+loader = DataLoader(dataset=dataset , batch_size=8 , shuffle=True)
+
+for batch in loader:
+    hr, lr1, lr2 = batch['HR'], batch['LR1'], batch['LR2']
+    print(hr.shape, lr1.shape, lr2.shape)
+    break
